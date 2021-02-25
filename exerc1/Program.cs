2f71b66831c778991c673f76ee54c81b367eea7f
Joao Velho <joaovictor.rv39@gmail.com>
2021-02-25 14:20:56 -0300

    exerc1 base working

--- exerc1/Program.cs
+++ exerc1/Program.cs
@@ -6,7 +6,19 @@ namespace exerc1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Multiplicando:");
+            int multiplicando = int.Parse(Console.ReadLine());
+
+            Console.Write("Início do intervalo:");
+            int inicio = int.Parse(Console.ReadLine());
+
+            Console.Write("Fim do intervalo:");
+            int fim = int.Parse(Console.ReadLine());
+
+            for (int i = inicio; i <= fim; i++) {
+                int result = multiplicando * i;
+                Console.WriteLine("{0} x {1} = {2}", multiplicando, i, result);
+            }
         }
     }
 }
