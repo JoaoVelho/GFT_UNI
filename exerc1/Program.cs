96b29f0c360ef0930806f2ac8328756cd8b4e156
Joao Velho <joaovictor.rv39@gmail.com>
2021-02-25 14:35:08 -0300

    exerc1 complete

--- exerc1/Program.cs
+++ exerc1/Program.cs
@@ -9,16 +9,45 @@ namespace exerc1
             Console.Write("Multiplicando:");
             int multiplicando = int.Parse(Console.ReadLine());
 
+            if (!CheckValid(multiplicando, "multiplicando")) {
+                return;
+            }
+
             Console.Write("Início do intervalo:");
             int inicio = int.Parse(Console.ReadLine());
 
+            if (!CheckValid(inicio, "inicio")) {
+                return;
+            }
+
             Console.Write("Fim do intervalo:");
             int fim = int.Parse(Console.ReadLine());
+
+            if (!CheckValid(fim, "fim")) {
+                return;
+            }
+
+            if (inicio > fim) {
+                Console.WriteLine("O início do intervalo deve ser menor que seu fim");
+                return;
+            } else if (fim - inicio >= 10) {
+                Console.WriteLine("O intervalo não pode ter uma diferença maior que 10");
+                return;
+            }
 
             for (int i = inicio; i <= fim; i++) {
                 int result = multiplicando * i;
                 Console.WriteLine("{0} x {1} = {2}", multiplicando, i, result);
             }
+        }
+
+        static bool CheckValid(int value, string valueName) 
+        {
+            if (value < 0 || value > 3000) {
+                Console.WriteLine("O {0} deve ser positivo e menor que 3001", valueName);
+                return false;
+            }
+            return true;
         }
     }
 }
